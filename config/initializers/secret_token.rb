c7422c6f12c16b373a1d3624e7823467ff4348bc
Christine McClure <camcclure@gmail.com>
2012-11-03 10:06:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iitdatabases::Application.config.secret_token = '8ea0adbdeb8e1db5adeed7caa6cfed0bcc1b46a3c8ffa70fbcf04eea17db1e5cfce403f44b68bc9963e9868dc3ac91b873541c870f5780c5d1b3b8b19607315e'
