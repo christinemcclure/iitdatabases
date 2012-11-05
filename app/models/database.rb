e0423e9f1da9102b8c9d3b1e7b5d3c796a99c3c3
Christine McClure <camcclure@gmail.com>
2012-11-04 21:31:19 -0600

    added has and belongs to many relationship between database and subject

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,3 +1,4 @@
 class Database < ActiveRecord::Base
   attr_accessible :access_url, :active, :alt_titles, :description, :notes, :popular, :restricted, :string, :title
+  has_and_belongs_to_many :subjects
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,4 @@
 class Subject < ActiveRecord::Base
   attr_accessible :iit_major, :notes, :title
+  has_and_belongs_to_many :databases
 end
