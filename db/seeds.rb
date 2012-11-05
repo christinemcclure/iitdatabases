1b5d342ab1f2e4e5e0a549e59eb9c44d5697031b
Christine McClure <camcclure@gmail.com>
2012-11-04 22:09:21 -0600

    removed bad string field from database form view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+vendor = Vendor.new(:name => "EBSCO")
+database = vendor.database.create([
+  { :title => "Academic Search Premier",
+    :alt_titles => "ASP",
+    :description => "Considered the world's largest multi-discipline full-text database of scholarly knowledge, this database is a good place to start searching for information on almost any topic. Covering all areas of academic study, this database contains indexing and abstracting for more than 8,200 magazines and journals, of which more than 3,600 are full-text peer-reviewed publications.",
+    :restricted => true,
+    :active => true,
+    :access_url => "http://search.EBSCOhost.com/login.asp?profile=web&defaultdb=aph",
+    :popular => true
+  }
+])
