64e3a8fcc13f434c23c8144bc4c73cf7914e9c14
Christine McClure <camcclure@gmail.com>
2012-11-04 21:55:26 -0600

    removed 'string' from database model'

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,5 +1,5 @@
 class Database < ActiveRecord::Base
-  attr_accessible :access_url, :active, :alt_titles, :description, :notes, :popular, :restricted, :string, :title, :vendor_id
+  attr_accessible :access_url, :active, :alt_titles, :description, :notes, :popular, :restricted, :title, :vendor_id
   has_one :vendor
   has_and_belongs_to_many :subjects
 end
