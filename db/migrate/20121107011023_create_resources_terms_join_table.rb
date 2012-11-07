1bf851e76e8a19d50f175aa8371d3df42992332c
Christine McClure <camcclure@gmail.com>
2012-11-06 19:11:46 -0600

    added join logic for resource and term migration

--- db/migrate/20121107011023_create_resources_terms_join_table.rb
+++ db/migrate/20121107011023_create_resources_terms_join_table.rb
@@ -1,5 +1,8 @@
 class CreateResourcesTermsJoinTable < ActiveRecord::Migration
   def up
+    create_table :resources_terms, :id => false do |t|
+      t.references :resource, :term
+    end
   end
 
   def down
