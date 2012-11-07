ba117aacefa9a9de42b66a969a59a21cfff7015f
Christine McClure <camcclure@gmail.com>
2012-11-06 19:12:37 -0600

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106214809) do
+ActiveRecord::Schema.define(:version => 20121107011023) do
 
   create_table "databases", :force => true do |t|
     t.string   "title"
@@ -32,12 +32,38 @@ ActiveRecord::Schema.define(:version => 20121106214809) do
     t.integer "subject_id"
   end
 
+  create_table "resources", :force => true do |t|
+    t.string   "title"
+    t.string   "alt_titles"
+    t.text     "description"
+    t.boolean  "restricted"
+    t.boolean  "active"
+    t.string   "access_url"
+    t.boolean  "popular"
+    t.text     "notes"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "resources_terms", :id => false, :force => true do |t|
+    t.integer "resource_id"
+    t.integer "term_id"
+  end
+
   create_table "subjects", :force => true do |t|
     t.string   "title"
     t.boolean  "iit_major"
     t.text     "notes"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "terms", :force => true do |t|
+    t.string   "item"
+    t.boolean  "iit_subject"
+    t.text     "notes"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "vendors", :force => true do |t|
