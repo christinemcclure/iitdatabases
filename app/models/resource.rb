0ecde00bd975e8cf9d7ada1079e2b2b939a8fb44
Christine McClure <camcclure@gmail.com>
2012-11-07 20:24:38 -0600

    added default sort order of title to resource  model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,4 +2,5 @@ class Resource < ActiveRecord::Base
   attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title
   has_and_belongs_to_many :terms
   validates :title, presence: true
+  default_scope :order => 'resources.title'
 end
