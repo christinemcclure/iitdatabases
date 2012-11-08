84e046213a6ce36a730443373630275d29ab523d
Christine McClure <camcclure@gmail.com>
2012-11-07 20:15:41 -0600

    made new owner column available in resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,4 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :access_url, :active, :alt_titles, :description, :notes, :popular, :restricted, :title
+  attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title
   has_and_belongs_to_many :terms
 end
