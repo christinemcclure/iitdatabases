aa685d23ea1172123c65f251767d2dceb48af7c1
Christine McClure <camcclure@gmail.com>
2012-11-07 19:46:27 -0600

    added ASP back to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,18 @@
 #])
 
 resource = Resource.create({
-  title: "My Resource",
-  description: "It's awesome"
+  :title => "Academic Search Premier",
+  :alt_titles => "ASP",
+  :description => "Considered the world's largest multi-discipline full-text database of scholarly knowledge, this database is a good place to start searching for information on almost any topic. Covering all areas of academic study, this database contains indexing and abstracting for more than 8,200 magazines and journals, of which more than 3,600 are full-text peer-reviewed publications.",
+  :restricted => true,
+  :active => true,
+  :access_url => "http://search.EBSCOhost.com/login.asp?profile=web&defaultdb=aph",
+  :popular => true
 })
 
 resource.terms.create([
-  { item: "Geography" },
-  { item: "History" },
-  { item: "Chemistry" }
+  { item: "general" },
+  { item: "history" }
 ])
 
 resource = Resource.create({
