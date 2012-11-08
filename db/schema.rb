0cfcacaa0b00eeeb601a396ec633d7336cbf08a3
Christine McClure <camcclure@gmail.com>
2012-11-07 20:12:11 -0600

    added owner column to resource model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108014853) do
+ActiveRecord::Schema.define(:version => 20121108021109) do
 
   create_table "resources", :force => true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20121108014853) do
     t.text     "notes"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "owner"
   end
 
   create_table "resources_terms", :id => false, :force => true do |t|
