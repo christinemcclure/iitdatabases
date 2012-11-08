7933b27750768d9d9336d8d5d1cd9663c1d8be9c
Christine McClure <camcclure@gmail.com>
2012-11-07 20:23:58 -0600

    added default sort order of item to term model

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,4 +1,6 @@
 class Term < ActiveRecord::Base
   attr_accessible :iit_subject, :item, :acronym, :notes
-    has_and_belongs_to_many :resources
+  has_and_belongs_to_many :resources
+  validates :item, presence: true
+  default_scope :order => 'terms.item'
 end
