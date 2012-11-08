348b579a2e1ba248d5bbd57b586da622cb9d4aaa
Christine McClure <camcclure@gmail.com>
2012-11-07 20:20:09 -0600

    validate title

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,5 @@
 class Resource < ActiveRecord::Base
   attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title
   has_and_belongs_to_many :terms
+  validates :title, presence: true
 end
