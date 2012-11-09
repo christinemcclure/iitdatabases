ed55e77e0587047f92124597d32fc4fe8c57c348
Christine McClure <camcclure@gmail.com>
2012-11-09 13:53:09 -0600

    added accepts_nested_attributes_for for terms to resource model, and included :terms_attributes in the accessible attributes

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,7 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title
+  attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title, :terms_attributes
   has_and_belongs_to_many :terms
+  accepts_nested_attributes_for :terms, :allow_destroy => true
   validates :title, presence: true
   default_scope :order => 'resources.title'
 end
