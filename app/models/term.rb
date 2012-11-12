47243e76131afd8d3d6b48ce9fe03129e0fa503e
Christine McClure <camcclure@gmail.com>
2012-11-11 19:50:23 -0600

    started skethcing out HABTM relationship

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,5 +1,6 @@
 class Term < ActiveRecord::Base
   attr_accessible :iit_subject, :item, :acronym, :notes
+#  has_and_belongs_to_many :resources, :class_name => "Term", :join_table => "resources", :association_foreign_key => ""
   has_and_belongs_to_many :resources
   validates :item, presence: true
   default_scope :order => 'terms.item'
