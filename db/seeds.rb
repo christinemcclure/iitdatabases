0baf7d63fd89dfbf0cb0583a3e5d77d11eca1d9a
Christine McClure <camcclure@gmail.com>
2012-11-11 18:26:32 -0600

    added realistic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,46 @@
 #  }
 #])
 
+Resource.delete_all
+Term.delete_all
+
+terms = Term.create ([
+  {:item => "airforce aerospace studies",:acronym => "AS",:iit_subject => true},
+  {:item => "archiecture",:acronym => "arch",:iit_subject => true},
+  {:item => "Architecture and Urbanism",:acronym => "aurb",:iit_subject => true},
+  {:item => "art and architectural history",:acronym => "aah",:iit_subject => true},
+  {:item => "biology",:acronym => "biol",:iit_subject => true},
+  {:item => "Biomedical Engineering",:acronym => "bme",:iit_subject => true},
+  {:item => "business",:acronym => "bus",:iit_subject => true},
+  {:item => "chemical engineering",:acronym => "che",:iit_subject => true},
+  {:item => "chemistry",:acronym => "chem",:iit_subject => true},
+  {:item => "Civil and Architectural Engineering",:acronym => "cae",:iit_subject => true},
+  {:item => "Communications",:acronym => "com",:iit_subject => true},
+  {:item => "Computer Science",:acronym => "cs",:iit_subject => true},
+  {:item => "Cooperative Education",:acronym => "coop",:iit_subject => true},
+  {:item => "Economics",:acronym => "econ",:iit_subject => true},
+  {:item => "Electrical and Computer Engineering",:acronym => "ece",:iit_subject => true},
+  {:item => "Engineering Graphics",:acronym => "eg",:iit_subject => true},
+  {:item => "english",:acronym => "eng",:iit_subject => true},
+  {:item => "Environmental Engineering",:acronym => "enve",:iit_subject => true},
+  {:item => "Environmental Management and Sustainability",:acronym => "ems",:iit_subject => true},
+  {:item => "Food Process Engineering",:acronym => "fpe",:iit_subject => true},
+  {:item => "Food Safety and Technology",:acronym => "fst",:iit_subject => true},
+  {:item => "Graduate Continuation Studies",:acronym => "gcs",:iit_subject => true},
+  {:item => "Industrial Technology and Management",:acronym => "intm",:iit_subject => true},
+  {:item => "Institute of Design",:acronym => "id",:iit_subject => true},
+  {:item => "humanities",:acronym => "hum",:iit_subject => true},
+  {:item => "information technology and management",:acronym => "itm",:iit_subject => true},
+  {:item => "political science",:acronym => "ps",:iit_subject => true},
+  {:item => "Mathematics",:acronym => "math",:iit_subject => true},
+  {:item => "Military Science",:acronym => "mils",:iit_subject => true},
+  {:item => "Naval Science",:acronym => "ns",:iit_subject => true},
+  {:item => "physics",:acronym => "phys",:iit_subject => true},
+  {:item => "Psychology",:acronym => "psyc",:iit_subject => true},
+  {:item => "sociology",:acronym => "soc",:iit_subject => true},
+  {:item => "general",:acronym => "",:iit_subject => false}
+])
+
 resource = Resource.create({
   :title => "Academic Search Premier",
   :alt_titles => "ASP",
@@ -27,18 +67,37 @@ resource = Resource.create({
   :access_url => "http://search.EBSCOhost.com/login.asp?profile=web&defaultdb=aph",
   :popular => true
 })
-
-resource.terms.create([
-  { item: "general" },
-  { item: "history" }
-])
+terms = ["general","political science","humanities","psychology","english","communications","business"]
+terms.each do |s|
+  resource.terms << Term.find_or_create_by_item(s)
+end
 
 resource = Resource.create({
-  title: "New Resource",
-  description: "It's really awesome"
+  :title => "ACM Digital Library",
+  :alt_titles => "Association for Computing Machinery",
+  :owner => "ACM Portal",
+  :description => "The Association for Computing Machinery Digital Library is an online repository of papers from publications that have been published, co-published, or co-marketed by the ACM, including journals, newsletters, and conference proceedings. All computing subject areas are covered.",
+  :restricted => true,
+  :active => true,
+  :access_url => "http://search.EBSCOhost.com/login.asp?profile=web&defaultdb=aph",
+  :popular => true
 })
+terms = ["computer science","communications"]
+terms.each do |s|
+  resource.terms << Term.find_or_create_by_item(s)
+end
 
-terms = ["Geography", "Chemistry", "Alchemy"]
+resource = Resource.create({
+  :title => "Business Source Premier",
+  :alt_titles => "BSP",
+  :owner => "EBSCO",
+  :description => "Business Source Premier, is the industry's most used business research database, providing full text for more than 2,300 journals, including full text for more than 1,100 peer-reviewed business publications. Full text coverage in all disciplines of business, including marketing, management, MIS, POM, accounting, finance and economics. Business Source Premier contains full text from the world's top management and marketing journals. The database also includes other sources of full text information such as country economic reports from the EIU, Global Insight, ICON Group and CountryWatch and detailed company profiles for the world's 10,000 largest companies. Additional full text, non-journal content includes market research reports, industry reports, country reports, company profiles and SWOT analyses.",
+  :restricted => true,
+  :active => true,
+  :access_url => "http://search.EBSCOhost.com/login.asp?profile=web&defaultdb=aph",
+  :popular => true
+})
+terms = ["general","political science","humanities","psychology","english","communications","business"]
 terms.each do |s|
   resource.terms << Term.find_or_create_by_item(s)
 end
