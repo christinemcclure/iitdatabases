288bbcc2b0c08aed05d613b28a00cd9689e333f6
Christine McClure <camcclure@gmail.com>
2012-11-12 21:38:15 -0600

    added term_ids to resource accessible attributes

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,5 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title, :terms_attributes
+  attr_accessible :access_url, :active, :alt_titles, :owner, :description, :notes, :popular, :restricted, :title, :terms_attributes, :term_ids
   has_and_belongs_to_many :terms
   accepts_nested_attributes_for :terms, :allow_destroy => true
   validates :title, presence: true
