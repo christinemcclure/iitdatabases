8088935cefabd1554b9ce1beaec566fa316f4d6c
Christine McClure <camcclure@gmail.com>
2012-12-01 09:17:49 -0600

    added OR acronym to join search

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -15,10 +15,10 @@ class Resource < ActiveRecord::Base
     if search
       search_condition = "%" + search + "%"
       #working without join
-      where(['title LIKE ? OR alt_titles LIKE ? OR description LIKE ? OR notes LIKE ?', search_condition, search_condition, search_condition, search_condition])
+      #where(['title LIKE ? OR alt_titles LIKE ? OR description LIKE ? OR notes LIKE ?', search_condition, search_condition, search_condition, search_condition])
 
       #below doesn't work. try different join
-      #where(['title LIKE ? OR alt_titles LIKE ? OR description LIKE ?', search_condition, search_condition, search_condition]).joins(:terms).where('item LIKE ?', search_condition)
+      joins(:terms).where('item LIKE ? OR acronym like ?', search_condition, search_condition)
 
     else
       find(:all)
