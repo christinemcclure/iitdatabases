df3e48695f875af398d07955f24f6b0df4d2fbc4
Christine McClure <camcclure@gmail.com>
2012-12-03 11:49:22 -0600

    added resource helpers for popular and restricted notes

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -2,4 +2,21 @@ module ResourcesHelper
   def myTruncate(myText)
       truncate(myText, :length => 160, :separator => ' ', :omission => "...")
   end
+
+  def restrictions_note
+    if @resource.restricted = true
+      restrictions_note = "Restricted to current IIT students, faculty and staff."
+    else
+      restrictions_note = "No restrictions."
+    end
+  end
+
+  def popular_note
+    if @resource.popular = true
+      restrictions_note = "Yes"
+    else
+      restrictions_note = "No"
+    end
+  end
+
 end
